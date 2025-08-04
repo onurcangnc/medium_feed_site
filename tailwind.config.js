40573a4ed1029787d7a60ce4bbc14ae41d6e8aff
unknown <onurcan.genc@ug.bilkent.edu.tr>
2025-08-04 20:26:11 +0300

    ignore some files4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["templates/**/*.html", "templates/**/*.j2"],
+  content: ["templates/**/*.html", "templates/**/*.j2", "templates/**/*.html.j2"],
   theme: {
     extend: {},
   },
