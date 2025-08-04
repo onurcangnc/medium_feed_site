872ecbccc66c8ff0ce1afca52c3a8218ab84b6b4
unknown <onurcan.genc@ug.bilkent.edu.tr>
2025-08-04 15:20:50 +0300

    test issues resolved.3

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,11 @@
+# tests/test_rss.py
+
 from main import extract_thumbnail, strip_tags
 
-def test_extract_thumbnail():
-    html = '<p><img src="https://i.ibb.co/fxzGdMH/1.jpg"/></p>'
-    assert extract_thumbnail(html) == "https://i.ibb.co/fxzGdMH/1.jpg"
+def test_thumbnail_extraction():
+    html = '<p><img src="https://example.com/image.jpg"/></p>'
+    assert extract_thumbnail(html) == "https://example.com/image.jpg"
 
-def test_strip_tags():
+def test_strip_tags_removes_html():
     html = "<p>Hello <strong>world</strong></p>"
     assert strip_tags(html) == "Hello world"
